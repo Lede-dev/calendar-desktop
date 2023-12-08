00c621faa9832fb42928974c2a5685f036ab8538
Lede-dev <lede.7716@gmail.com>
2023-12-09 03:07:51 +0900

    Feat: 이벤트 객체 저장 경로 설정

--- src/jvmMain/kotlin/net/ledestudio/calendar/manager/CalendarManagerBuilder.kt
+++ src/jvmMain/kotlin/net/ledestudio/calendar/manager/CalendarManagerBuilder.kt
@@ -2,14 +2,14 @@ package net.ledestudio.calendar.manager
 
 import net.ledestudio.calendar.event.CalendarEventHolderImpl
 import net.ledestudio.calendar.storage.CalendarEventStorage
-import java.nio.file.Path
+import java.nio.file.Paths
 
 class CalendarManagerBuilder {
 
     private val manager = CalendarManagerImpl()
 
     fun initCalendarEventHolder(): CalendarManagerBuilder {
-        val storage = CalendarEventStorage(Path.of(""))
+        val storage = CalendarEventStorage(Paths.get("", "data/events"))
         val holder = CalendarEventHolderImpl(storage)
         manager.setCalendarEventHolder(holder)
         return this
