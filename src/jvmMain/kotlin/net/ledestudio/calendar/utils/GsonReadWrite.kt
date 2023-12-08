487ac7c362d22389677c1c0d12c670b57175aef4
Lede-dev <lede.7716@gmail.com>
2023-12-09 03:28:33 +0900

    Feat: ZonedDateTime 객체를 직렬화/역직렬화 하도록 Gson Serializer/Deserializer 추가

--- src/jvmMain/kotlin/net/ledestudio/calendar/utils/GsonReadWrite.kt
+++ src/jvmMain/kotlin/net/ledestudio/calendar/utils/GsonReadWrite.kt
@@ -1,12 +1,17 @@
 package net.ledestudio.calendar.utils
 
 import com.google.gson.GsonBuilder
+import net.ledestudio.calendar.utils.serializer.ZonedDateTimeSerializer
 import java.io.File
 import java.nio.charset.StandardCharsets
+import java.time.ZonedDateTime
 
 object GsonReadWrite {
 
-    private val gson = GsonBuilder().setPrettyPrinting().create()
+    private val gson = GsonBuilder()
+        .setPrettyPrinting()
+        .registerTypeAdapter(ZonedDateTime::class.java, ZonedDateTimeSerializer())
+        .create()
 
     fun <T> File.readObject(clazz: Class<T>): T = gson.fromJson(this.readText(StandardCharsets.UTF_8), clazz)
 
---
+++ src/jvmMain/kotlin/net/ledestudio/calendar/utils/serializer/ZonedDateTimeSerializer.kt
@@ -0,0 +1,25 @@
+package net.ledestudio.calendar.utils.serializer
+
+import com.google.gson.*
+import net.ledestudio.calendar.utils.ZonedDateTimeKR
+import java.lang.reflect.Type
+import java.time.ZonedDateTime
+
+class ZonedDateTimeSerializer: JsonSerializer<ZonedDateTime>, JsonDeserializer<ZonedDateTime> {
+
+    override fun serialize(src: ZonedDateTime?, typeOfSrc: Type?, context: JsonSerializationContext?): JsonElement {
+        if (src != null) {
+            return JsonPrimitive(ZonedDateTimeKR.serialize(src))
+        }
+        return JsonPrimitive("")
+    }
+
+    override fun deserialize(json: JsonElement?, typeOfT: Type?, context: JsonDeserializationContext?): ZonedDateTime {
+        if (json != null) {
+            val str = json.asJsonPrimitive.asString
+            return if (str.isEmpty()) ZonedDateTime.now() else ZonedDateTimeKR.deserialize(str)
+        }
+        return ZonedDateTime.now()
+    }
+
+}
