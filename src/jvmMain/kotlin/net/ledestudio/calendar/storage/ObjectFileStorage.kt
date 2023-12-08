3e0ad334e823966ce751df30258b0845a5463e32
Lede-dev <lede.7716@gmail.com>
2023-12-09 00:35:25 +0900

    Feat: 특정 경로의 보관함(Dir)에 파일을 저장/로드하는 기능 정의

---
+++ src/jvmMain/kotlin/net/ledestudio/calendar/storage/ObjectFileStorage.kt
@@ -0,0 +1,15 @@
+package net.ledestudio.calendar.storage
+
+import java.nio.file.Path
+
+interface ObjectFileStorage<T> {
+
+    fun getPath(): Path
+
+    fun save(name: String, obj: T)
+
+    fun load(name: String): T
+
+    fun load(): List<T>
+
+}
