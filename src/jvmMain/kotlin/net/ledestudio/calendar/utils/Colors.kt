746f4bd203bb3dcb9e35a4558f2492b64afa0d87
Lede-dev <lede.7716@gmail.com>
2023-12-09 04:17:28 +0900

    Feat: HexCode를 Color로 변경하는 기능 추가

---
+++ src/jvmMain/kotlin/net/ledestudio/calendar/utils/Colors.kt
@@ -0,0 +1,11 @@
+package net.ledestudio.calendar.utils
+
+import androidx.compose.ui.graphics.Color
+
+object Colors {
+
+    fun Color.fromHex(hexString: String): Color {
+        return Color(("ff" + hexString.removePrefix("#").lowercase()).toLong(16))
+    }
+
+}
