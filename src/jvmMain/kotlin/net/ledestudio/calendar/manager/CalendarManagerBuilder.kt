e233d7aff3484e302600d6d1ee6e8d6912567d24
Lede-dev <lede.7716@gmail.com>
2023-12-09 02:14:56 +0900

    Feat: 캘린더 매니저 빌더에서 캘린더 이벤트 홀더를 초기화

--- src/jvmMain/kotlin/net/ledestudio/calendar/manager/CalendarManagerBuilder.kt
+++ src/jvmMain/kotlin/net/ledestudio/calendar/manager/CalendarManagerBuilder.kt
@@ -1,7 +1,19 @@
 package net.ledestudio.calendar.manager
 
+import net.ledestudio.calendar.event.CalendarEventHolderImpl
+import net.ledestudio.calendar.storage.CalendarEventStorage
+import java.nio.file.Path
+
 class CalendarManagerBuilder {
 
+    private val manager = CalendarManagerImpl()
 
+    fun initCalendarEventHolder() {
+        val storage = CalendarEventStorage(Path.of(""))
+        val holder = CalendarEventHolderImpl(storage)
+        manager.setCalendarEventHolder(holder)
+    }
+
+    fun build() = manager
 
 }
