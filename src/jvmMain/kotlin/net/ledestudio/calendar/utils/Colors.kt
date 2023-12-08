0408f2a322bc1c21f1596761d9a52234d76c2974
Lede-dev <lede.7716@gmail.com>
2023-12-09 04:19:54 +0900

    Feat: 기본 색상 팔레트 추가

--- src/jvmMain/kotlin/net/ledestudio/calendar/utils/Colors.kt
+++ src/jvmMain/kotlin/net/ledestudio/calendar/utils/Colors.kt
@@ -4,6 +4,11 @@ import androidx.compose.ui.graphics.Color
 
 object Colors {
 
+    val background = fromHex("#333333")
+    val text = fromHex("#CCCCCC")
+    val icon = fromHex("#CCCCCC")
+    val highlight = fromHex("#2ecc71")
+
     fun fromHex(hexString: String): Color {
         return Color(("ff" + hexString.removePrefix("#").lowercase()).toLong(16))
     }
