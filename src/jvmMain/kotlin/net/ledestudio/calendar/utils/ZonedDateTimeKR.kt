9a514a1a57d09351766b0183b44e940aa7aad545
Lede-dev <lede.7716@gmail.com>
2023-12-09 02:02:04 +0900

    Feat: 날짜 범위 비교 유틸 추가

--- src/jvmMain/kotlin/net/ledestudio/calendar/utils/ZonedDateTimeKR.kt
+++ src/jvmMain/kotlin/net/ledestudio/calendar/utils/ZonedDateTimeKR.kt
@@ -16,5 +16,19 @@ object ZonedDateTimeKR {
 
     fun of(year: Int, month: Int, day: Int, hour:Int, minute: Int): ZonedDateTime =
         ZonedDateTime.of(year, month, day, hour, minute, 0, 0, zoneId)
-    
+
+    fun checkCross(
+        firstStartTime: ZonedDateTime,
+        firstExpireTime: ZonedDateTime,
+        secondStartTime: ZonedDateTime,
+        secondExpireTime: ZonedDateTime): Boolean
+    {
+        return firstStartTime.isInRange(secondStartTime, secondExpireTime) ||
+                    firstExpireTime.isInRange(secondStartTime, secondExpireTime) ||
+                        secondStartTime.isInRange(firstStartTime, firstExpireTime) ||
+                            secondExpireTime.isInRange(firstStartTime, firstExpireTime)
+    }
+
+    fun ZonedDateTime.isInRange(first: ZonedDateTime, second: ZonedDateTime): Boolean =
+        this.isEqual(first) || this.isEqual(second) || this.isAfter(first) || this.isBefore(second)
 }
