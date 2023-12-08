7896a6336e8089d5794d56e270dd2d905229704a
Lede-dev <lede.7716@gmail.com>
2023-12-09 01:31:29 +0900

    Feat: 캘린더 이벤트 데이터 클래스에 uuid 필드 추가

--- src/jvmMain/kotlin/net/ledestudio/calendar/data/CalendarEvent.kt
+++ src/jvmMain/kotlin/net/ledestudio/calendar/data/CalendarEvent.kt
@@ -1,8 +1,10 @@
 package net.ledestudio.calendar.data
 
 import java.time.ZonedDateTime
+import java.util.UUID
 
 data class CalendarEvent(
+    val uuid: UUID,
     var startAt: ZonedDateTime,
     var expireAt: ZonedDateTime,
     var title: String,
