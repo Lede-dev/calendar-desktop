3f1ddc2976ba4e704479e6ceaa3d8d571c2bd388
Lede-dev <lede.7716@gmail.com>
2023-12-10 02:08:03 +0900

    Feat: 더 많은 기본 컬러 추가

--- src/jvmMain/kotlin/net/ledestudio/calendar/utils/Colors.kt
+++ src/jvmMain/kotlin/net/ledestudio/calendar/utils/Colors.kt
@@ -4,13 +4,28 @@ import androidx.compose.ui.graphics.Color
 
 object Colors {
 
-    val background = fromHex("#333333")
+    val navBackground = fromHex("#333333")
+    val navIcon = fromHex("#00FF00")
+
+    val calendarBackground = fromHex("#222222")
+    val calendarContent = fromHex("#333333")
+    val calendarSepLine = fromHex("#555555")
+    val calendarContentText = fromHex("#CCCCCC")
+    val calendarTitle = fromHex("#00FF00")
+
+
     val text = fromHex("#CCCCCC")
     val icon = fromHex("#CCCCCC")
     val highlight = fromHex("#2ecc71")
 
+    val transparent = Color(0, 0, 0, 0)
+
     fun fromHex(hexString: String): Color {
         return Color(("ff" + hexString.removePrefix("#").lowercase()).toLong(16))
+    }
+
+    fun fromHexAlpha(hexString: String): Color {
+        return Color(hexString.removePrefix("#").lowercase().toLong(16))
     }
 
 }
